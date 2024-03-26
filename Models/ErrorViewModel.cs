5cb786dbccfca099ae8f934835fa41508671a667
mathewd41 <mathewd4@mymacewan.ca>
2024-03-26 17:36:04 -0600

    commit1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AmazingWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
